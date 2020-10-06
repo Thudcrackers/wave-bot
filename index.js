4f26e2e6f64ab6a02f33e505c50183902f461273
Jack Morton <jmort87@gmail.com>
2020-10-06 15:20:33 -0400

    .

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ express().listen(PORT, () => console.log("Listening on port " + PORT))
 
 bot.on('message', message => {
     console.log(message.channel.id)
-    console.log(message.mentions)
-    if (message.channel.id === 763107608868683877 && message.mentions.size > 0) {
+    console.log(message.mentions.users.size)
+    if (message.channel.id === 763107608868683877 && message.mentions.users.size > 0) {
         message.react("👋");
     }
 });
