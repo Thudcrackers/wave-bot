94dca01dc756f8ececd8df998859720607f1f979
Jack Morton <jmort87@gmail.com>
2020-10-06 15:14:45 -0400

    Test message channel toString

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.login(TOKEN);
 express().listen(PORT, () => console.log("Listening on port " + PORT))
 
 bot.on('message', message => {
-    //console.log(message.author)
+    console.log(message.channel.toString())
     if (message.channel.toString() == '#welcome' && message.mentions.size > 0) {
         message.react("👋");
     }
